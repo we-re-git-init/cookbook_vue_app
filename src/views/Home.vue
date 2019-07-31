ea6f4d5c8577831adf90e2ec1127e0bf85d96824
Brian <riceb53@gmail.com>
2019-07-30 20:26:23 -0500

    add frontend new/create

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <p>Title: <input type="text" v-model="newRecipeTitle"></p>
+    <p>Directions: <input type="text" v-model="newRecipeDirections"></p>
+    <p>Ingredients: <input type="text" v-model="newRecipeIngredients"></p>
+    <p>ImageUrl: <input type="text" v-model="newRecipeImageUrl"></p>
+    <p>PrepTime: <input type="text" v-model="newRecipePrepTime"></p>
+    <button v-on:click="makeRecipe()">Make a new recipe</button>
     <div v-for="recipe in recipes">
       <p>title:{{recipe.title}}</p>
       <p>image url: {{recipe.image_url}}</p>
@@ -20,7 +26,12 @@ export default {
   data: function() {
     return {
       message: "change.js!",
-      recipes: []
+      recipes: [],
+      newRecipeTitle: "",
+      newRecipeDirections: "",
+      newRecipeIngredients: "",
+      newRecipeImageUrl: "",
+      newRecipePrepTime: ""
     };
   },
   created: function() {
@@ -30,7 +41,32 @@ export default {
       this.recipes = response.data;
     })
   },
-  methods: {}
+  methods: {
+    makeRecipe: function() {
+      console.log('in make recipe');
+      // get some data
+      var newRecipe = {
+        title: this.newRecipeTitle,
+        ingredients: this.newRecipeIngredients,
+        directions: this.newRecipeDirections,
+        prep_time: this.newRecipePrepTime,
+        image_url: this.newRecipeImageUrl
+      }
+      // send that data to the api
+      axios.post('/api/recipes', newRecipe).then(response => {
+        console.log('in the callback for create')
+        console.log(response.data);
+        this.recipes.push(response.data);
+      })
+    }
+  }
 };
+
+// user enters information
+// user clicks button
+// get all information that user has typed in
+// send that data to the api
+// show the user the new item they have created
+
 </script>
 
